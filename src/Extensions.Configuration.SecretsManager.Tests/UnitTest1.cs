35a018fc979bd50f66a4d8c8e533fc612a63c316
Scott Offen <118468886+sdoffen@users.noreply.github.com>
2024-12-11 11:40:56 -0700

    Initial commit

---
+++ src/Extensions.Configuration.SecretsManager.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Extensions.Configuration.SecretsManager;
+
+namespace Extensions.Configuration.SecretsManager.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ src/Extensions.Configuration.SecretsManager/README.md
@@ -0,0 +1,6 @@
+# Extensions.Configuration.SecretsManager
+
+<!--
+Provide an overview of what your class library does and how to get started.
+This file will be published with the nuget package
+-->
